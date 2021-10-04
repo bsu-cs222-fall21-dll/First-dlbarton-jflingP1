c7baa064699ac539687d505b2cdea6eb1cd09d51
lingj <jfling@bsu.edu>
2021-10-04 13:32:03 -0400

    Adds test for AccessWikipedia

---
+++ src/test/java/edu/bsu/testers/AccessWikipediaTest.java
@@ -0,0 +1,13 @@
+package edu.bsu.testers;
+
+import edu.bsu.Model.AccessWikipedia;
+import org.junit.jupiter.api.Test;
+
+public class AccessWikipediaTest {
+
+    @Test
+    public void testMakeUrl(){
+        AccessWikipedia accessWikipedia = new AccessWikipedia();
+
+    }
+}
